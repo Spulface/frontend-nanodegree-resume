4b46379eb5083188c1a4e5580526194d8f3d6900
Martin Freyberg <Spulface@aol.com>
2017-10-10 13:51:23 +0200

    latest state

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,76 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+"name" : "Martin Freyberg",
+"role": "Technical Account Manager",
+"contact" : {
+	"mobile" : "+49151123124",
+	"twitter" : "twitter.com/Spulface",
+	"email" : "Spulface@aol.com",
+	"location" : "Munich, Germany"
+},
+"pictureUrl" : "images/me.png",
+"welcomeMsg" : "Hey there, it's Martin!",
+"skills" : ["not", "so", "cool"]
+};
+
+var work = {
+"jobs" : [
+	{
+		"title" : "Technical Account Manager",
+		"employer" : "AWIN AG",
+		"dates" : "07/2016 - current",
+		"location" : "Munich, Germany",
+		"description" : "FPOC for Customer Tech Integrations"
+	},
+	{
+		"title" : "Product Manager",
+		"employer" : "Shore GmbH",
+		"dates" : "01/2016 - 06/2016",
+		"location" : "Munich, Germany",
+		"description" : "Project Lead - Payment Processor Integration"
+	}
+]};
+
+var projects = {
+"projects" : [
+	{
+		"title" : "Dynamic Adcode Structuring",
+		"dates" : "07/2017 - current",
+		"description" : "Allowing A-grade customers to use dynamic target URLs for any given affilinet adcode.",
+		"images" : []
+	},
+	{
+		"title" : "Stripe Payment Processor Integration",
+		"dates" : "02/2016 - 06/2016",
+		"description" : "Providing a B2B integration (Stripe API + own frontend) that allows end customers to process their payments through a wite label Stripe integration.",
+		"images" : []
+	}
+]};
+
+
+var education = {
+"schools" : [
+	{
+		"name" : "University of Applied Sciences",
+		"dates" : "2008 - 2012",
+		"location" : "Munich, Germany",
+		"majors" : ["Business Administration"]
+	},
+	{
+		"name" : "Berufsoberschule München Nord",
+		"dates" : "2007 - 2008",
+		"location" : "Munich, Germany"
+	},
+	{	
+		"name" : "Realschule Tutzing",
+		"dates" : "1999 - 2004",
+		"location" : "Tutzing, Germany"
+	}
+],
+"onlineCourses" : [
+	{
+		"title": "JavaScript Basics",
+		"school": "UDACITY",
+		"dates": "09/2017 - current",
+		"url": "https://de.udacity.com/course/javascript-basics--ud804/"
+	}
+]};
